88c2969f1621699b41e00f85e05b46358b24e295
Brad Thompson <gbradthompson@gmail.com>
2018-11-30 01:03:04 -0700

    auction house health done

--- pages/health.vue
+++ pages/health.vue
@@ -4,23 +4,33 @@
       <div class="text-xs-center">
         <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       </div>
-      <v-card >
-        <v-card-title class="headline">Auction House Health</v-card-title>
+      <v-card>
+        <v-card-title class="headline">Realm Health</v-card-title>
         <v-card-text>
           <v-select :items="regions" v-model="region" label="Region"></v-select>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="requestData">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card v-if="listings.length > 0" class="mt-5">
+        <v-card-title class="headline">Realm Health</v-card-title>
+        <v-card-text>
           <v-data-table
-            :headers="statsHeadings"
-            :items="stats"
+            :headers="listingsHeadings"
+            :items="listings"
             class="elevation-1"
             :rows-per-page-items="[ 50, 100, 500 ]"
+            must-sort
+            :pagination.sync="pagination"
           >
             <template slot="items" slot-scope="props">
-              <td>{{props.item.auctionHouse}}</td>
-              <td v-html="goldStyling(props.item.marketCap)"></td>
-              <td>{{props.item.volume}}</td>
-              <td>{{Math.round(props.item.percentSold)}}%</td>
-              <td>{{Math.round(props.item.percentExpired)}}%</td>
-              <td>{{Math.round(props.item.percentCanceled)}}%</td>
+              <td>{{ props.item.name }}</td>
+              <td><display-gold :value="props.item.liveMarketCap"></display-gold></td>
+              <td>{{ props.item.liveVolume }}</td>
+              <td><display-gold :value="props.item.sellPriceAvg"></display-gold></td>
+              <td>{{msToTimeString(props.item.sellRate)}}</td>
             </template>
           </v-data-table>
         </v-card-text>
@@ -30,74 +40,81 @@
 </template>
 
 <style>
-.copper, .silver, .gold {
-  font-weight: bold;
-  padding-left:2px;
-  font-size: 10px;
-}
-.copper {
-  color: #b87333;
-}
-.silver {
-  color: silver;
-}
-.gold {
-  color: gold;
-  font-size: 16px;
-}
 </style>
 
 <script>
   export default {
     computed: {
-      healthHeadings () {
-        if (this.health.length > 0) return Object.keys(this.health[0]).map(head => {return {value: head, text: head}})
-        else return []
+      regions () {
+        return Object.keys(this.$store.state.realmIndex)
+        .map(r => this.$store.state.realmIndex[r].regionTag)
+        .reduce((a, v) => { if (!a.includes(v)) a.push(v); return a }, [])
       },
-      statsHeadings () {
-        if (this.stats.length > 0) return Object.keys(this.stats[0]).map(head => {return {value: head, text: head}})
-        else return []
-      },
-      stats () {
-        return this.health.filter(h => {
-          return h.region === this.region
-        }).map(h => {
-          let auctionHouse = h.ah
-          let marketCap = h.sold_buyout_sum + h.expired_buyout_sum + h.canceled_buyout_sum
-          let volume = h.sold_num + h.expired_num + h.canceled_num
-          let percentSold = (100/volume) * h.sold_num
-          let percentExpired = (100/volume) * h.expired_num
-          let percentCanceled = (100/volume) * h.canceled_num
-          return {auctionHouse, marketCap, volume, percentSold, percentExpired, percentCanceled}
+      auctionHouseNameLookup () {
+        let ahLookup = {}
+        Object.keys(this.$store.state.realmIndex).forEach(index => {
+          let realm = this.$store.state.realmIndex[index]
+          if (typeof ahLookup[realm.ahid] === 'undefined') ahLookup[realm.ahid] = []
+          ahLookup[realm.ahid].push(realm.name)
         })
+        Object.keys(ahLookup).forEach(index => {
+          ahLookup[index] = ahLookup[index].join(', ')
+        })
+        return ahLookup
+      },
+      auctionHouseRegionLookup () {
+        let ahLookup = {}
+        Object.keys(this.$store.state.realmIndex).forEach(index => {
+          let realm = this.$store.state.realmIndex[index]
+          ahLookup[realm.ahid] = realm.regionTag
+        })
+        return ahLookup
+      },
+      listings () {
+        let list = []
+        this.listingsRaw
+        .filter(item => {
+          return this.auctionHouseRegionLookup[item.ahid] === this.region
+        })
+        .forEach(item => {
+          let name = this.auctionHouseNameLookup[item.ahid]
+          list.push(Object.assign(item, {name}))
+        })
+
+        return list
       }
     },
     data () {
       return {
         region: 'US',
-        regions: ['US', 'EU', 'KR', 'TW']
+        listingsRaw: [],
+        pagination: {descending: false, page: 0, rowsPerPage: -1, sortBy: 'sellRate'},
+        listingsHeadings: [
+          {text: 'Name', value: 'name'},
+          {text: 'Market Cap', value: 'liveMarketCap'},
+          {text: 'Volume', value: 'liveVolume'},
+          {text: 'Avg Sell', value: 'sellPriceAvg'},
+          {text: 'Sell Rate', value: 'sellRate'}
+        ]
       }
     },
     methods: {
-      numberWithCommas (x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      async requestData (event) {
+        this.listingsRaw = []
+        this.listingsRaw = await this.$axios.$get(`http://localhost:3303/health`)
       },
-      goldStyling (num) {
-        let str = '' + Math.round(num)
-        let copper = str.substring(str.length -2, str.length)
-        let silver = str.substring(str.length -4, str.length -2)
-        let gold = str.substring(0, str.length -4)
-
-        let styled = ''
-        if (str.length > 4) styled += `<span class="gold">${this.numberWithCommas(gold)}</span>`
-        if (str.length > 2) styled += `<span class="silver">${silver}</span>`
-        styled += `<span class="copper">${copper}</span>`
-        return styled
+      msToTimeString (ms) {
+        let str = ''
+        let hours = Math.floor(ms / (1000*60*60))
+        ms = ms % (1000*60*60)
+        let minutes = Math.floor(ms / (1000*60))
+        ms = ms % (1000*60)
+        let seconds = Math.floor(ms / (1000))
+        ms = ms % (1000)
+        if (hours) return `${hours}h ${minutes}m ${seconds}s`
+        if (minutes) return `${minutes}m ${seconds}s`
+        if (seconds) return `${seconds}s`
       }
-    },
-    async asyncData({ app }) {
-      const health = await app.$axios.$get('http://54.244.210.52:3303/health')
-      return { health }
     }
   }
 </script>
