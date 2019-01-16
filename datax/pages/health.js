d2dc55721632cae8f34344d8eec11a2e83f6b862
Brad Thompson <gbradthompson@gmail.com>
2019-01-15 17:59:56 -0700

    add half price to projection

--- datax/pages/health.js
+++ datax/pages/health.js
@@ -8,7 +8,25 @@ class Health {
 
   async request (query) {
     let db = await kaisBattlepets.getDB()
-    let health = await db.collection('auctionHouseHealth').find({}, {projection: {_id: 0, ahid: 1, liveMarketCap: 1, liveVolume: 1, sellPriceAvg: 1, sellRate: 1, lastUpdate: 1, soldOfOneThousand: 1, expiredOfOneThousand: 1, canceledOfOneThousand: 1}}).toArray()
+    let health = await db.collection('auctionHouseHealth').find({}, {projection: {
+      _id: 0,
+      ahid: 1,
+      liveMarketCap: 1,
+      liveVolume: 1,
+      sellPriceAvg: 1,
+      sellRate: 1,
+      lastUpdate: 1,
+      soldOfOneThousand: 1,
+      expiredOfOneThousand: 1,
+      canceledOfOneThousand: 1,
+      halfPriceUnique: 1,
+      halfPriceUniqueOverOneHundredThousand: 1,
+      halfPriceUniqueOverTenThousand: 1,
+      halfPriceUniqueOverOneThousand: 1,
+      halfPriceUniqueOverOneHundred: 1,
+      halfPriceUniqueOverTen: 1,
+      halfPriceUniqueOverZero: 1
+    }}).toArray()
 
     return health
   }
