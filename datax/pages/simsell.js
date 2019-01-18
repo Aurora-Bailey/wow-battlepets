4c5ff2e5362de64807a3474a5b0c2b1ed1f63a2f
Brad Thompson <gbradthompson@gmail.com>
2019-01-18 15:17:09 -0700

    sim sell

---
+++ datax/pages/simsell.js
@@ -0,0 +1,114 @@
+const MongoDB = require('../api/mongodb.js')
+const kaisBattlepets = new MongoDB('kaisBattlepets')
+const wow = require('../api/wow.js')
+const lib = require('../api/lib.js')
+
+class Collection {
+  constructor () {
+
+  }
+
+  async request (query) {
+    query.rid = parseInt(query.rid)
+    query.canceled = parseInt(query.canceled) || 100 // number of canceled items to allow before skipping the pet
+    query.discount = parseInt(query.discount) || 1 // number of canceled items to allow before skipping the pet
+    // get pets from blizzard
+    let pets = await wow.getCharacterPets(query.rid, query.name)
+
+    // resolve realm id
+    let db = await kaisBattlepets.getDB()
+    let realm = await db.collection('realmIndex').findOne({id: query.rid})
+    if (realm === null) throw 'Realm not found!'
+
+    // pullin averages
+    for (var index in pets) {
+      let pet = pets[index]
+      let level = parseInt(pet.stats.level)
+      if (level !== 25) level = 1
+      pet.speciesId = pet.stats.speciesId
+      pet.average = await lib.speciesAverageRegion(pet.speciesId, level, realm.regionTag)
+      if (pet.average === null) pet.average = {sold: {median: 0, num: 0}}
+    }
+
+    // simplify
+    let petsSimple = pets.map(pet => {
+      return {
+        psid: pet.stats.speciesId,
+        level: pet.stats.level,
+        price: pet.average.sold.median,
+        sold: pet.average.sold.num,
+        guid: pet.battlePetGuid,
+        quality: pet.stats.petQualityId
+      }
+    })
+
+    // build ahid lookup
+    let auctionHouseIndex = await db.collection('auctionHouseIndex').find({}, {projection: {_id: 0, ahid: 1, regionTag: 1}}).toArray()
+    let ahidToRegionTag = auctionHouseIndex.reduce((a, v) => {
+      a[v.ahid] = v.regionTag
+      return a
+    }, {})
+
+    // filter unique
+    let petSimpleObject = petsSimple.reduce((a, v) => {
+      a[v.psid] = v
+      return a
+    }, {})
+    let petSimpleUnique = Object.keys(petSimpleObject).map(key => petSimpleObject[key])
+
+    // Auction house compare object
+    let auctionHouseCompare = []
+
+    for (var i = 0; i < 7; i++) {
+      console.log(`Day: ${i}`)
+      await (async function () {
+        let fromTime = Date.now() - ((i+1) * 1000 * 60 * 60 * 24)
+        let toTime = Date.now() - (i * 1000 * 60 * 60 * 24)
+        let auctionsArchive = await db.collection('auctionsArchive').find({$or: [{status: 'sold'}, {status: 'canceled'}], lastSeen: {$gte: fromTime, $lt: toTime}},
+        {projection: {_id: 0, buyout: 1, petSpeciesId: 1, petQualityId: 1, petLevel: 1, status: 1, ahid: 1}}).toArray()
+        console.log('-db')
+
+        // filter out all but player region
+        auctionsArchive = auctionsArchive.filter(item => ahidToRegionTag[item.ahid] === realm.regionTag)
+
+        // count sold above threshold and number of canceled pets organized by ahid and psid
+        let soldCanceled = {} // {ahid: {psid: {sold: 0, canceled: 0}, ...}, ...}
+        auctionsArchive.forEach(item => {
+          if (typeof petSimpleObject[item.petSpeciesId] === 'undefined') return false
+          if (item.status === 'sold' && item.buyout > (petSimpleObject[item.petSpeciesId].price * query.discount)) {
+            if (typeof soldCanceled[item.ahid] === 'undefined') soldCanceled[item.ahid] = {}
+            if (typeof soldCanceled[item.ahid][item.petSpeciesId] === 'undefined') soldCanceled[item.ahid][item.petSpeciesId] = {sold: 0, canceled: 0}
+            soldCanceled[item.ahid][item.petSpeciesId]['sold']++
+          }else if (item.status === 'canceled') {
+            if (typeof soldCanceled[item.ahid] === 'undefined') soldCanceled[item.ahid] = {}
+            if (typeof soldCanceled[item.ahid][item.petSpeciesId] === 'undefined') soldCanceled[item.ahid][item.petSpeciesId] = {sold: 0, canceled: 0}
+            soldCanceled[item.ahid][item.petSpeciesId]['canceled']++
+          }
+        })
+
+        // count sum price and count of all valid sold pets
+        let sumNum = {} //{ahid: {sum: 0, num: 0}, ...}
+        Object.keys(soldCanceled).forEach(key => {
+          let soldCanceledAhid = soldCanceled[key]
+          if (typeof sumNum[key] === 'undefined') sumNum[key] = {sum: 0, num: 0}
+          Object.keys(soldCanceledAhid).forEach(key2 => {
+            let soldCanceledAhidPsid = soldCanceledAhid[key2]
+            if (soldCanceledAhidPsid['canceled'] > query.canceled) return false
+            let itemPsid = parseInt(key2)
+            sumNum[key]['num']++
+            sumNum[key]['sum'] += petSimpleObject[itemPsid].price
+          })
+        })
+
+        auctionHouseCompare.push(sumNum)
+
+      })()
+    }
+
+    // get sold history
+
+    return {dev: auctionHouseCompare}
+  }
+}
+
+module.exports = new Collection()
--- datax/server.js
+++ datax/server.js
@@ -1,15 +1,3 @@
-const buy = require('./pages/buy.js')
-const multibuy = require('./pages/multibuy.js')
-const collection = require('./pages/collection.js')
-const health = require('./pages/health.js')
-const pet = require('./pages/pet.js')
-const petindex = require('./pages/petindex.js')
-const player = require('./pages/player.js')
-const name = require('./pages/name.js')
-const realmindex = require('./pages/realmindex.js')
-const sell = require('./pages/sell.js')
-const selltime = require('./pages/selltime.js')
-
 const chalk = require('chalk')
 const express = require('express')
 const app = express()
@@ -21,58 +9,81 @@ app.use(function(req, res, next) {
   next()
 })
 
+const buy = require('./pages/buy.js')
 app.get('/buy/:ahid', async function (req, res, next) {
   try { res.json(await buy.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
+const multibuy = require('./pages/multibuy.js')
 app.get('/multibuy/:rid/:name/:buyat', async function (req, res, next) {
   try { res.json(await multibuy.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
+const collection = require('./pages/collection.js')
 app.get('/collection/:rid/:name', async function (req, res, next) {
   try { res.json(await collection.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
+const health = require('./pages/health.js')
 app.get('/health', async function (req, res, next) {
   try { res.json(await health.request(req.query)) }
   catch (e) { next(e) }
 })
 
+const pet = require('./pages/pet.js')
 app.get('/pet/:psid', async function (req, res, next) {
   try { res.json(await pet.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
+const petindex = require('./pages/petindex.js')
 app.get('/petindex', async function (req, res, next) {
   try { res.json(await petindex.request(req.query)) }
   catch (e) { next(e) }
 })
 
+const player = require('./pages/player.js')
 app.get('/player/:ahid/:name', async function (req, res, next) {
   try { res.json(await player.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
+const name = require('./pages/name.js')
 app.get('/name/:name', async function (req, res, next) {
   try { res.json(await name.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
+const realmindex = require('./pages/realmindex.js')
 app.get('/realmindex', async function (req, res, next) {
   try { res.json(await realmindex.request(req.query)) }
   catch (e) { next(e) }
 })
 
+const sell = require('./pages/sell.js')
 app.get('/sell/:rid/:name/:sellat', async function (req, res, next) {
   try { res.json(await sell.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
+const selltime = require('./pages/selltime.js')
 app.get('/selltime', async function (req, res, next) {
   try { res.json(await selltime.request(Object.assign(req.params, req.query))) }
+  catch (e) { next(e) }
+})
+
+const simsell = require('./pages/simsell.js')
+app.get('/simsell/:rid/:name', async function (req, res, next) {
+  try { res.json(await simsell.request(Object.assign(req.params, req.query))) }
+  catch (e) { next(e) }
+})
+
+const simsellbuy = require('./pages/simsellbuy.js')
+app.get('/simsellbuy/:rid/:name', async function (req, res, next) {
+  try { res.json(await simsellbuy.request(Object.assign(req.params, req.query))) }
   catch (e) { next(e) }
 })
 
