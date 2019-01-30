801806c386ca8b0945bf7c5a912ced5daa11e627
Brad Thompson <gbradthompson@gmail.com>
2019-01-29 20:40:56 -0700

    output soldNum

--- datax/pages/name.js
+++ datax/pages/name.js
@@ -12,7 +12,7 @@ class Player {
     query.hours = parseInt(query.hours) || 24
 
     let db = await kaisBattlepets.getDB()
-    let options = {projection: {_id: 0, ahid: 1, lastSeen: 1, buyout: 1, median: 1, petLevel: 1, petSpeciesId: 1, petQualityId: 1, status: 1, timeLeft: 1}}
+    let options = {projection: {_id: 0, ahid: 1, lastSeen: 1, buyout: 1, median: 1, petLevel: 1, petSpeciesId: 1, petQualityId: 1, status: 1, timeLeft: 1, soldNum: 1}}
     let live = await db.collection('auctionsLive').find({owner: query.name, status: query.status}, options).toArray()
     let archive = await db.collection('auctionsArchive').find({owner: query.name, status: query.status, lastSeen: {$gte: Date.now() - (1000*60*60*query.hours)}}, options).toArray()
     return live.concat(archive)
